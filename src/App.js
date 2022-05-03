b6bf8ddbe6ab7383ccbb595276431036eb9c0be9
モガ太郎 <mogashiro@gmail.com>
2022-05-03 21:45:19 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React, { Component } from 'react';
 
+// class App extends Component {
+//   render() {
+//     return <div className="App">Hello, World!</div>;
+//   }
+// }
 
-function App() {
-  return (
-    <div className="App"><h1>Hello, World!</h1></div>
-  );
+class App extends Component {
+  render() {
+    return React.createElement(
+      'div',
+      null,
+      'Hello, world!!!'
+    )
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App"><h1>Hello, World!</h1></div>
+//   );
+// }
 
 export default App;
